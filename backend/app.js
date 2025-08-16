cd314eaaf154ae716b155fa3917c5e136f6b1de5
bipin-newar <newar.boy.55555@gmail.com>
2025-08-16 16:46:20 +0100

    Add backend with CORS

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Configure CORS to allow requests from the frontend
+app.use(cors({
+  origin: 'https://happy-island-0f80c1103.2.azurestaticapps.net',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
